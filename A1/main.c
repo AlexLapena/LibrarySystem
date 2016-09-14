b5763e201f0db24859e212051a583b31d7ed0142
AlexLapena <alapena@mail.uoguelph.ca>
2016-09-13 20:30:11 -0400

    [misc] File I/O started

--- A1/main.c
+++ A1/main.c
@@ -13,6 +13,7 @@
 int main(int argc, char **argv)
 {
     struct dataHeader *dH;
+    struct dataHeader *read;
     struct dataString *dS;
     char *nameReturn;
 
@@ -20,7 +21,7 @@ int main(int argc, char **argv)
     setName(dH, "This is the Name");
     nameReturn = getName(dH);
 
-    printf("%s\n", nameReturn);
+    printf("%s\n\n", nameReturn);
 
     addString(dH, "string");
     addString(dH, "another");
@@ -34,11 +35,26 @@ int main(int argc, char **argv)
 
     freeStructure(dH);
 
+    printf("\nprocess string\n");
     dH = buildHeader();
-    setName(dH, "This is the Name");
+    setName(dH, "name");
     nameReturn = getName(dH);
-    addString(dH, "Free");
-    printString(dH);
+    addString(dH, "test\nend");
+    processStrings(dH);
+    freeStructure(dH);
+
+    printf("\nfile io\n");
+    dH = buildHeader();
+    setName(dH, "name");
+    addString(dH, "file test");
+    addString(dH, "test 2.0");
+    writeStrings("test.bin", dH);
+    read = readStrings("test.bin");
+    printString(read);
+
+
+    freeStructure(dH);
+    freeStructure(read);
 
     return(0); 
 } 
--- A1/src/liblistio.c
+++ A1/src/liblistio.c
@@ -65,6 +65,8 @@ void printString(struct dataHeader *header)
 {
     struct dataString *dS = header->next;
 
+    printf("%s\n", header->name);
+
     while (dS != NULL) {
         printf("%s\n", dS->string);
         dS = dS->next;
@@ -73,7 +75,19 @@ void printString(struct dataHeader *header)
 
 void processStrings(struct dataHeader *header)
 {
+    /* FIX ME */
+    struct dataString *dS = header->next;
+    int length = strlen(dS->string);
+    int i;
 
+    printf("%s\n",dS->string);
+    for (i = 0; i <= length; i++) {
+        if ((dS->string[i-1] == dS->string[i]) && (dS->string[i] == (char)32 || 
+                dS->string[i] == '\n')) {
+            
+            printf("got here %d\n", i);
+        }
+    }
 }
 
 void freeStructure(struct dataHeader *header)
@@ -94,10 +108,34 @@ void freeStructure(struct dataHeader *header)
 
 void writeStrings(char *filename, struct dataHeader *header)
 {
+    // Test me
+    struct dataString * dS = header->next;
+    FILE *fp;
+    int nameLength, strLength;
 
+    fp = fopen(filename, "wb");
+
+    nameLength = strlen(header->name);
+    fwrite(&nameLength, 1, sizeof(int), fp);
+    fwrite(header->name, 1, sizeof(header->name), fp);
+    fwrite(&header->length, 1, sizeof(int), fp);
+
+    while (dS != NULL) {
+        strLength = strlen(dS->string);
+        fwrite(&strLength, 1, sizeof(int), fp);
+        fwrite(dS->string, 1, sizeof(dS->string), fp);
+        dS = dS->next;
+    }
+    fclose(fp);
 }
 
 struct dataHeader *readStrings(char *filename)
 {
+    struct dataHeader *header;
+    struct
+    FILE * fp;
 
+    fp = fopen(filename, "rb");
+
+    fclose(fp);
 }
